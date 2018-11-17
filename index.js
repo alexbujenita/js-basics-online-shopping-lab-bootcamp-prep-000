5c61ee3d756965d34a31034d5e1d6fdb8cbe50be
dekc89 <dekc89@gmail.com>
2018-11-17 12:17:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,22 +45,22 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for (let i = 0; i < cart.length; i++) {
-    if (cart[i].itemName === item) {
-        cart.splice(i, 1);
-    }
-  }
-  if (cart.length) {
-    return cart;
-  }
-  return "That item is not in your cart.";
-  // const index = cart.findIndex(x => x.itemName === item);
-  //   if (index === - 1) {
-  //   return "That item is not in your cart.";
+  // for (let i = 0; i < cart.length; i++) {
+  //   if (cart[i].itemName === item) {
+  //       cart.splice(i, 1);
+  //   }
   // }
-  // const temp = cart.splice(index, 1);
-  //
-  // return cart;
+  // if (cart.length) {
+  //   return cart;
+  // }
+  // return "That item is not in your cart.";
+  const index = cart.findIndex(x => x.itemName === item);
+    if (index === - 1) {
+    return "That item is not in your cart.";
+  }
+  const temp = cart.splice(index, 1);
+  
+  return cart;
 }
 
 function placeOrder(cardNumber) {
