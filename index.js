21c62d6255d0fb3874a274922534a8853cc8ec22
dekc89 <dekc89@gmail.com>
2018-11-17 12:37:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,5 +64,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+  if (!cardNumber) {
+    return "Sorry, we don't have a credit card on file for you.";
+  }
+  cart.length = 0;
+  return `Your total cost is ${total()}, which will be charged to the card 83296759.`
   // write your code here
 }
