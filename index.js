9d67ad720fed4d07e972fb33437c46a746b3a982
dekc89 <dekc89@gmail.com>
2018-11-17 12:43:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,8 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }
-  // cart.length = 0;
-  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  var text = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+  cart.length = 0;
+  return text;
   // write your code here
 }
